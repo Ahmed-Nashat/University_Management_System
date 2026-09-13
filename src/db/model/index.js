0320110045179f7e3890036d48f1025180a36c0d
Ahmed-Nashat <ahmadnashat04@gmail.com>
2026-09-13 12:21:01 +0300

    feat(semester): add semester model

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -3,3 +3,5 @@ export { CurseModel } from "./course.model.js";
 export { ProfessorModel } from "./professor.model.js";
 export { DepartmentModel } from "./department.mode.js";
 export { SectionModel } from "./section.model.js";
+export { EnrollmentModel } from "./enrollment.mode.js";
+export { SemesterModel } from "./semester.model.js";
