0828735ec3da710e24fa8fad6fe0ffbf7b4ae8f1
Ahmed-Nashat <ahmadnashat04@gmail.com>
2026-09-13 11:53:57 +0300

    feat(section): add section model

---
+++ src/db/model/section.model.js
@@ -0,0 +1,57 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../connection.js";
+
+export class SectionModel extends Model {
+  id;
+}
+
+SectionModel.init(
+  {
+    room: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+      },
+      set(value) {
+        this.setDataValue("room", value.trim());
+      },
+    },
+    schedule: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sectionCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "section_code",
+    },
+    semesterId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      field: "semester_id",
+    },
+    courseId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "course_id",
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: true,
+    paranoid: true,
+    sequelize,
+    indexes: [
+      {
+        unique: true,
+        fields: ["section_code", "semester_id", "course_id"],
+        name: "unique_section_per_course_semester",
+      },
+    ],
+  },
+);
