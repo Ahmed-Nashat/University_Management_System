bcc953b5adc51a7f80ee099dde795265216fa0e4
Ahmed-Nashat <ahmadnashat04@gmail.com>
2026-09-16 18:23:07 +0300

    feat(enrollment): add enrollment API endpoints

--- src/module/index.js
+++ src/module/index.js
@@ -1,3 +1,4 @@
+export { enrollmentRouter } from "./enrollment/enrollment.controller.js";
 export { semesterRouter } from "./semester/semester.controller.js";
 export { courseRouter } from "./course/course.controller.js";
 export { departmentRouter } from "./department/department.controller.js";
